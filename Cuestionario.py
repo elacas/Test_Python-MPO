8ac4e6f01cc3eec7ef4cdf580cb30c0a6ef26032
elacas <eli_dkf_@hotmail.com>
2025-07-18 22:38:59 +0200

    18/07

--- Cuestionario.py
+++ Cuestionario.py
@@ -1,3 +1,55 @@
+'''
+import json (para que el programa "lea y concierta a Python el archivo json)
+import random (importar el "progama para randomizar preguntas)
+
+'''
+import json
+import random
+
+'''
+Definir cargar peguntas con (with) abrir el json, R read/lectura, "diccionario" utf8 (tiene en cuenta los simbolos como la ñ), se abre como/as archivo (devuelve el json leido como archivo)
+'''
+def cargar_preguntas():
+   with open ("preguntas.json", "r", encoding="utf-8") as archivo:
+      return json.load(archivo)
+
+'''
+Definir asignaturas diferentes: print= se muestran las variables disponibles, con input se le dice a la persona que escoja asignatura o volver al menu
+'''     
+def elegir_asignatura():
+
+   print("\nEscoge asignatura")
+   print("\n1-Lenguaje de Marcas")
+   print("\n2-Fundamentos del Hardware")
+   print("\n3-MPO-Python")
+   print("\n4-Bases de Datos")
+   print("\n5-Itinerario para la Empleabilidad")
+   print("\n6-Implantación Sistemas Operativos")
+   print("\n7-Planificacion y Administración de Redes")
+  
+   opcion = input("Escoja asignatura")
+   while True:    
+     if opcion == "1":
+         return "Lenguaje_de_Marcas"
+     elif opcion == "2":
+         return "Fundamentos_del_Hardware"
+     elif opcion == "3":
+         return "MOP_Python"
+     elif opcion == "4":
+         return "Bases_Datos"
+     elif opcion == "5":
+         return "Itinerario_empleabilidad"
+     elif opcion == "6":
+         return "Implantacion_Sistemas"
+     elif opcion == "7":
+         return "Planificacion_de_Redes"
+     elif opcion == "8":
+         return "main"
+     else:
+       print("Porfavor, introduzca un umero válido")
+       return elegir_asignatura()
+
+
 def main():
     print("="*25,"TEST ASIR", "="*25)
     print()
